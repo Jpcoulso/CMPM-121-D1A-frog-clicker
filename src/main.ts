13c67ff7c4c1ee922accf6a4f6406179905f9176
Joseph Coulson <jpcoulso@ucsc.edu>
2025-10-11 08:30:38 +0000

    added a button that is clickable, changed its css so it looks like a frog (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+// Create the button element
+const button = document.createElement("button");
+//add css class "frogClicker" to button
+button.classList.add('frogClicker');
+
+// Add a click event listener
+button.addEventListener("click", () => {
+  console.log("Button was clicked!");
+});
+
+// Append it to the body (or any container)
+document.body.appendChild(button);
