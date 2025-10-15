b18b3a9fd436de1edc8f1df7901335120e70905f
Joseph Coulson <jpcoulso@ucsc.edu>
2025-10-15 20:35:46 +0000

    Added a <div> that displays 'count' variable, each time frog button is clicked count is increased by one and display is updated. (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,30 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+// create counter display
+const display = document.createElement("div");
+display.textContent = "0";
+// append display to body so it shows up
+document.body.appendChild(display);
+
+// create count variable to track number of frogs
+let count: number = 0;
+
 // Create the button element
 const button = document.createElement("button");
 //add css class "frogClicker" to button
 button.classList.add("frogClicker");
 
-// Add a click event listener
-button.addEventListener("click", () => {
-  console.log("Button was clicked!");
-});
-
 // Append it to the body (or any container)
 document.body.appendChild(button);
+
+
+// Add a click event listener
+// On click increase count by 1
+button.addEventListener("click", () => {
+  count++;
+  display.textContent = `${count} Frogs`;
+});
+
+
